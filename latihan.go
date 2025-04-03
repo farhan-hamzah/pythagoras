6730c9df8935aefc871414c5a3581f856fa1b71a
Farhan Hamzah <177802321+farhan-hamzah@users.noreply.github.com>
2025-04-03 11:00:06 +0700

    Add files via upload

---
+++ latihan.go
@@ -0,0 +1,21 @@
+// package main
+// import "fmt"
+
+// func pythgoras(a, b, c int)bool{
+// 	var cek bool
+// 	a = a*a
+// 	b = b*b
+// 	c = c*c
+
+// 	if c == a+b {
+// 		cek = true
+// 	}else{
+// 		cek = false
+// 	}
+// 	return cek
+// }
+// func main(){
+// 	var a,b,c int
+// 	fmt.Scan(&a, &b, &c)
+// 	fmt.Print(pythgoras(a, b, c))
+// }
